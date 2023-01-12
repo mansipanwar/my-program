bb5899e62abae9b4c68365052aab061144865c28
mansipanwar <118964134+mansipanwar@users.noreply.github.com>
2023-01-12 19:19:34 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void myFunction()
+{
+    cout<<"I am in function"<<endl;
+}
+int main()
+{
+    cout<<"I am in main program"<<endl;
+    myFunction();
+    return 0;
+}
